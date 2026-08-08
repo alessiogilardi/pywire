4791f1f37033ceeffb221797a41462bb47a02495
Alessio Gilardi <alessio.gilardi@globant.com>
2026-08-08 15:43:53 +0200

    📦 Export public API

---
+++ src/pywire/__init__.py
@@ -0,0 +1,25 @@
+from .container import Container
+from .decorators import (
+    agent,
+    client,
+    component,
+    get_global_container,
+    repository,
+    service,
+)
+from .definitions import BeanDefinition
+from .exceptions import DependencyResolutionError
+from .markers import Autowired
+
+__all__ = [
+    "Autowired",
+    "BeanDefinition",
+    "Container",
+    "DependencyResolutionError",
+    "agent",
+    "client",
+    "component",
+    "get_global_container",
+    "repository",
+    "service",
+]
